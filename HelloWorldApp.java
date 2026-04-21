20ba2875ae48d9f44bd8690742f006226e7e64ca
gurjarsumit366-svg <gurjarsumit366@gmail.com>
2026-04-21 21:10:10 +0530

    UC7: Use String.join to display multiple names

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -2,17 +2,8 @@ public class HelloWorldApp {
     public static void main(String[] args) {
 
         if (args.length > 0) {
-            String result = "Hello ";
-
-            for (String name : args) {
-                result += name + " ";
-            }
-
-            // remove last space
-            result = result.substring(0, result.length() - 1);
-
-            System.out.println(result);
-
+            String result = String.join(" ", args);
+            System.out.println("Hello " + result);
         } else {
             System.out.println("Hello, World!");
         }
