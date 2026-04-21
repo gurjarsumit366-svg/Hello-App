98ef8c6e82cedf282e41f7e3cb7b14e1b42e9ed5
gurjarsumit366-svg <gurjarsumit366@gmail.com>
2026-04-21 21:01:21 +0530

    UC5 code

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -3,13 +3,13 @@ public class HelloWorldApp {
 
         if (args.length > 0) {
             System.out.print("Hello ");
-            
-            for (int i = 0; i < args.length; i++) {
-                System.out.print(args[i] + " ");
+
+            for (String name : args) {
+                System.out.print(name + " ");
             }
 
         } else {
-            System.out.println("Hello World");
+            System.out.println("Hello, World!");
         }
     }
 }
